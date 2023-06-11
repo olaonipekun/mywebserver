a15534a297adc62421c417a3dad7aa9dba6b8275
Ololade Olaonipekun <olaonipekunololade94@gmail.com>
2023-06-11 20:55:56 +0000

    prisma global integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule],
+  imports: [UserModule, BookmarkModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
-  signin() {
+  constructor(private prisma: PrismaService) {}
+  signin(@Body() dto: any) {
+    console.log({
+      dto,
+    });
     return ' I have signed in ';
   }
 
